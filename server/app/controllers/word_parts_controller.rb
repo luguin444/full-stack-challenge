c35f42e4716020c5fb0d66081f87a6776166984a
Luis Ganimi <luguin444@gmail.com>
2025-02-17 17:20:59 -0300

    feat: server route and action to update status

--- server/app/controllers/word_parts_controller.rb
+++ server/app/controllers/word_parts_controller.rb
@@ -4,4 +4,18 @@ class WordPartsController < ApplicationController
 
     render json: @phonics_level.word_parts
   end
+
+  def update
+    @word_part = WordPart.find(params[:id])
+
+    status = params[:status]
+
+    unless WordPart.statuses.keys.include?(status)
+      render json: { error: "Status not allowed" }, status: :unprocessable_entity
+      return
+    end
+
+    @word_part.update(status: status)
+    render json: @word_part, status: :ok
+  end
 end
